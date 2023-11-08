9ec0d5715e35d01ab6516a65b0605fd4b96f8d9c
Russell Warner <rewarner@russellbits.com>
2023-11-07 20:23:09 -0500

    Feature: New routes created.

---
+++ src/routes/lab/+page.svelte
@@ -0,0 +1,4 @@
+<!-- lab/+page.svelte -->
+<h1>The Laboratory</h1>
+
+<p>I put experiments with javascript here</p>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,4 @@
+<!-- projects/+page.svelte -->
+<h1>Projects</h1>
+
+<p>These are projects I have built for reasons.</p>
