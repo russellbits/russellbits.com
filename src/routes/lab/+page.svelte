5595543327c31e976aca2bc4c10902bea66e5d3d
Russell Warner <rewarner@russellbits.com>
2023-12-20 16:18:05 -0500

    Feature: added routes for individual lab experiments.

---
+++ src/routes/lab/+page.svelte
@@ -0,0 +1,9 @@
+<!-- lab/+page.svelte -->
+<h1>Lab</h1>
+
+<p>This is where the experiments go.</p>
+
+<ul>
+	<li><a href="game-of-life">Game of life</a></li>
+	<li><a href="shrinky-box">Shrinky Box</a></li>
+</ul>
