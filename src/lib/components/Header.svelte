006ba938cd1fe39e43e6ae8cad8e1abf12a67408
Russell Warner <rewarner@russellbits.com>
2024-04-27 12:44:33 -0400

    Logo re-sizes for @media, menu shrinks to hamburger for mobile.

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,34 @@
 <script>
 	import '$lib/styles/entypo.css';
+	import { onMount } from 'svelte';
+
+	// Show mobile icon and display menu
+	let showMobileMenu = false;
+
+	// List of navigation items
+	const navItems = [
+		{ label: 'Blog', href: 'blog' },
+		{ label: 'Projects', href: 'projects' },
+		{ label: 'Lab', href: 'lab' }
+	];
+
+	// Mobile menu click event handler
+	const handleMobileIconClick = () => (showMobileMenu = !showMobileMenu);
+
+	// Media match query handler
+	const mediaQueryHandler = (e) => {
+		// Reset mobile state
+		if (!e.matches) {
+			showMobileMenu = false;
+		}
+	};
+
+	// Attach media query listener on mount hook
+	onMount(() => {
+		const mediaListener = window.matchMedia('(max-width: 767px)');
+
+		mediaListener.addListener(mediaQueryHandler);
+	});
 </script>
 
 <!-- Header.svelte -->
@@ -11,24 +40,25 @@
 
 <header>
 	<nav>
-		<ul>
-			<li>
-				<a href="/blog">Blog</a>
-			</li>
-			<li>
-				<a href="/projects">Projects</a>
-			</li>
-			<li>
-				<a href="/lab">Lab</a>
-			</li>
-			<li>
-				<a href="http://pinterest.com/belovedleader"><i class="glyph-pinterest glyph-white" /></a>
-			</li>
-			<li><a href="http://github.com/russellbits"><i class="glyph-github glyph-white" /></a></li>
-			<li>
-				<a href="http://instagram.com/belovedleader"><i class="glyph-instagram glyph-white" /></a>
-			</li>
-		</ul>
+		<div class="inner">
+			<div on:click={handleMobileIconClick} class={`mobile-icon${showMobileMenu ? ' active' : ''}`}>
+				<div class="middle-line" />
+			</div>
+			<ul class={`navbar-list${showMobileMenu ? ' mobile' : ''}`}>
+				{#each navItems as item}
+					<li>
+						<a href={item.href}>{item.label}</a>
+					</li>
+				{/each}
+				<li>
+					<a href="http://pinterest.com/belovedleader"><i class="glyph-pinterest glyph-white" /></a>
+				</li>
+				<li><a href="http://github.com/russellbits"><i class="glyph-github glyph-white" /></a></li>
+				<li>
+					<a href="http://instagram.com/belovedleader"><i class="glyph-instagram glyph-white" /></a>
+				</li>
+			</ul>
+		</div>
 	</nav>
 </header>
 
@@ -44,6 +74,147 @@
 		display: flex;
 		flex-wrap: wrap;
 		justify-content: flex-end;
+	}
+
+	#logo {
+		position: fixed;
+		top: 0px;
+		left: -100px;
+		z-index: 4000;
+	}
+
+	@media (max-width: 900px) {
+		#logo {
+			transform: scale(0.5);
+			top: -120px;
+			left: -160px;
+		}
+	}
+
+	.inner {
+		max-width: 980px;
+		padding-left: 20px;
+		padding-right: 20px;
+		margin: auto;
+		box-sizing: border-box;
+		display: flex;
+		align-items: center;
+		height: 100%;
+	}
+
+	.mobile-icon {
+		width: 25px;
+		height: 14px;
+		position: relative;
+		cursor: pointer;
+	}
+
+	.mobile-icon:after,
+	.mobile-icon:before,
+	.middle-line {
+		content: '';
+		position: absolute;
+		width: 100%;
+		height: 2px;
+		background-color: #fff;
+		transition: all 0.4s;
+		transform-origin: center;
+	}
+
+	.mobile-icon:before,
+	.middle-line {
+		top: 0;
+	}
+
+	.mobile-icon:after,
+	.middle-line {
+		bottom: 0;
+	}
+
+	.mobile-icon:before {
+		width: 66%;
+	}
+
+	.mobile-icon:after {
+		width: 33%;
+	}
+
+	.middle-line {
+		margin: auto;
+	}
+
+	.mobile-icon:hover:before,
+	.mobile-icon:hover:after,
+	.mobile-icon.active:before,
+	.mobile-icon.active:after,
+	.mobile-icon.active .middle-line {
+		width: 100%;
+	}
+
+	.mobile-icon.active:before,
+	.mobile-icon.active:after {
+		top: 50%;
+		transform: rotate(-45deg);
+	}
+
+	.mobile-icon.active .middle-line {
+		transform: rotate(45deg);
+	}
+
+	.navbar-list {
+		display: none;
+		width: 100%;
+		justify-content: space-between;
+		margin: 0;
+		padding: 0 40px;
+	}
+
+	.navbar-list.mobile {
+		background-color: rgba(0, 0, 0, 0.8);
+		position: fixed;
+		display: block;
+		height: calc(100% - 45px);
+		bottom: 0;
+		left: 0;
+	}
+
+	.navbar-list li {
+		list-style-type: none;
+		position: relative;
+	}
+
+	.navbar-list li:before {
+		content: '';
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		width: 100%;
+		height: 1px;
+		background-color: #424245;
+	}
+
+	.navbar-list a {
+		color: #fff;
+		text-decoration: none;
+		display: flex;
+		height: 45px;
+		align-items: center;
+		padding: 0 10px;
+	}
+
+	@media only screen and (min-width: 767px) {
+		.mobile-icon {
+			display: none;
+		}
+
+		.navbar-list {
+			display: flex;
+			padding: 0;
+		}
+
+		.navbar-list a {
+			display: inline-flex;
+		}
 	}
 
 	nav > ul > li {
