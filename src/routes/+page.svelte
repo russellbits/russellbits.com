7ef8b7130e3133c2ce40085eda298bfb122a78c3
Russell Warner <rewarner@russellbits.com>
2024-04-27 18:27:58 -0400

    Added new project display image, updated copy.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
 <h1>Hello. I am Russell Warner,</h1>
+<p>
+	I am a user interface architect, providing consultation on everything from design to
+	implementation, build and distribution.
+</p>
 
 <img
-	src="/images/projects/seqminaut-project-display.png"
+	src="/images/projects/seqseco-project-display.png"
 	width="1200"
 	height="1697"
 	alt="Project Seqseco on iphone"
