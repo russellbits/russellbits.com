b665eef0de746c8d51c7199a53d7d87ab3e672d0
Russell Warner <rewarner@russellbits.com>
2024-04-28 13:04:17 -0400

    Grid layout created on home page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,22 @@
-<h1>Hello. I am Russell Warner,</h1>
-<p>
-	I am a user interface architect, providing consultation on everything from design to
-	implementation, build and distribution.
-</p>
+<div />
+<div>
+	<h1>Hello.</h1>
+	<h2>I am Russell Warner.</h2>
+	<p>
+		I am a user interface architect, providing consultation on everything from design to
+		implementation, build and distribution.
+	</p>
+</div>
 
-<img
-	src="/images/projects/seqseco-project-display.png"
-	width="1200"
-	height="1697"
-	alt="Project Seqseco on iphone"
-/>
+<div><h1>Seqseco: A Product Design Story</h1></div>
+<div>
+	<img
+		src="/images/projects/seqseco-project-display.png"
+		width="1200"
+		height="1697"
+		alt="Project Seqseco on iphone"
+	/>
+</div>
+
+<style lang="scss">
+</style>
