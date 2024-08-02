716acc81bf2c33687c804abec661473488ff66ec
Russell Warner <rewarner@russellbits.com>
2024-08-01 21:36:18 -0400

    Feature: clarified names of blog components.

---
+++ src/lib/components/BlogPost.svelte
@@ -0,0 +1,56 @@
+<script>
+	import Tag from '$lib/components/Tag.svelte';
+
+	export let title = 'Blog Title';
+	export let blog_image = 'images/';
+	export let desc;
+</script>
+
+<div class="post">
+	<figure><img src={blog_image} alt={title} /></figure>
+	<div class="content">
+		<h2>{title}</h2>
+		<p>{desc}</p>
+		<div class="taglist">
+			<Tag name="Supabase" />
+			<Tag name="HTML 5" />
+			<Tag name="Javascript" />
+		</div>
+	</div>
+</div>
+
+<style>
+	.post {
+		margin: 0 0 2em 0;
+		padding: 0;
+		display: flex;
+		flex-direction: row; /* or column */
+		align-items: flex-start;
+		border: 1px solid purple;
+	}
+	.post h2 {
+		line-height: 1em;
+		margin: 0;
+	}
+	.post p {
+		margin: 0;
+	}
+	.post figure {
+		margin: 0 1em 0 0;
+		width: 280px;
+		height: 280px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		overflow: hidden;
+		flex-shrink: 0;
+	}
+	img {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+	}
+	.taglist {
+		margin-top: 10px;
+	}
+</style>
---
+++ src/lib/components/BlogSidebar.svelte
@@ -0,0 +1,22 @@
+<script>
+	// List code here
+</script>
+
+<h2>Popular Posts</h2>
+
+<ul>
+	<li>The Problem With Free Social Media</li>
+	<li>Raskins Non-skeumorphic Temperature Converter</li>
+	<li>Revenge of the Blink Tool</li>
+</ul>
+
+<style lang="scss">
+	ul {
+		padding: 0; /* Optional: Remove default padding */
+		margin: 0; /* Optional: Remove default margin */
+	}
+	li {
+		list-style-type: decimal;
+		list-style-position: outside;
+	}
+</style>
