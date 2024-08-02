fd4cf7519f7d21cdb85b8beecbc55c059e753da6
Russell Warner <rewarner@russellbits.com>
2024-08-01 21:40:33 -0400

    Feature: new attributes for project components.

--- src/lib/components/Project.svelte
+++ src/lib/components/Project.svelte
@@ -15,6 +15,26 @@
 	</div>
 </div>
 
+<!--
+@component
+## Project
+Creates a project card for the site
+
+@example
+```svelte
+<Project
+			title="My Project"
+			project_image="images/projects/my-project-sm.png"
+			desc="I designed this project."
+		/>
+```
+
+### Slot props
+- `title` - A single text element used in sev3eral places in the component tenplate
+- `project_image` - A url for the location of the project image
+- `desc` - A text description of the project.
+-->
+
 <style>
 	img {
 		max-width: 100%;
