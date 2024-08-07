0c9aa12f3776f930192e3b87076b9a74f50640be
Russell Warner <rewarner@russellbits.com>
2024-08-07 15:53:41 -0400

    Feature: page transitions in main layout.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,20 @@
 <!-- +layout.svelte -->
 <script>
+	import { fade } from 'svelte/transition';
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import '@picocss/pico';
 	import '$lib/styles/house.scss';
+	export let data;
 </script>
 
 <Header />
 
-<slot />
+{#key data.currentRoute}
+	<main in:fade={{ duration: 200, delay: 150 }} out:fade={{ duration: 150 }}>
+		<slot />
+	</main>
+{/key}
 
 <Footer />
 
