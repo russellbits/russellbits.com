e5d0725ba03dd7bb68b54da87bc97a601b3e9f25
Russell Warner <rewarner@russellbits.com>
2024-08-07 12:47:03 -0400

    Removed helper and api route for projects; projects will be built from static JSON file.

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -38,23 +38,3 @@ export const fetchMarkdownExperiments = async () => {
 
 	return allExperiments;
 };
-
-export const fetchMarkdownProjects = async () => {
-	const allProjectFiles = import.meta.glob('/src/routes/projects/*/*.md');
-	const iterableProjectFiles = Object.entries(allProjectFiles);
-
-	const allProjects = await Promise.all(
-		iterableProjectFiles.map(async ([path, resolver]) => {
-			// @ts-ignore
-			const { metadata } = await resolver();
-			const projectPath = path.slice(11, -8);
-
-			return {
-				meta: metadata,
-				path: projectPath
-			};
-		})
-	);
-
-	return allProjects;
-};
