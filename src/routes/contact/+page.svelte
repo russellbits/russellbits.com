eb045000a07ce9d9592c3af4927fef0c3495db65
Russell Warner <rewarner@russellbits.com>
2024-08-29 22:57:47 -0400

    Feature: new contact form supabase connection. Can report messages and insert messages into tables.

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,5 +1,43 @@
 <!-- contact/+page.svelte -->
 <script>
+	export let data
+	import { onMount } from 'svelte'
+	// import { createClient } from '@supabase/supabase-js'
+	import { supabase } from '$lib/supabaseClient'
+	import Title from '$lib/components/Title.svelte'
+
+	// console.log(data)
+
+	onMount(() => {
+		const form = document.querySelector('#contact-form')
+		form?.addEventListener('submit', async (event) => {
+			event.preventDefault()
+			const formInputs = form.querySelectorAll('input, textarea')
+			let submission = {}
+			// 	email: 'rewarner@russellbits.com',
+			// 	sender_name: 'Russell',
+			// 	message: 'Hello there.',
+			// 	sent: '2024-08-09T12:04:38Z'
+			// }
+			formInputs.forEach((element) => {
+				// @ts-ignore
+				const { value, name } = element
+				if (value) {
+					// @ts-ignore
+					submission[name] = value
+				}
+			})
+			console.log(submission)
+			const { error, data } = await supabase
+				.from('russellbits_messages')
+				.insert(submission)
+				.select()
+
+			// .insert(submission)
+
+			console.log({ error, data })
+		})
+	})
 </script>
 
 <svelte:head>
@@ -7,29 +45,65 @@
 	<meta property="og:title" content="Contact" />
 </svelte:head>
 
-<h1>Contact</h1>
-
-<p>reach out and say Hi.</p>
-
-<form>
-	<!-- Grid -->
-	<div class="grid">
-		<!-- Markup example 1: input is inside label -->
-		<label for="firstname">
-			First name
-			<input type="text" id="firstname" name="firstname" placeholder="First name" required />
-		</label>
-
-		<label for="lastname">
-			Last name
-			<input type="text" id="lastname" name="lastname" placeholder="Last name" required />
-		</label>
+<section>
+	<div class="header">
+		<Title title="Contact" color="blue" align="right" />
 	</div>
-	<!-- Markup example 2: input is after label -->
-	<label for="email">Email address</label>
-	<input type="email" id="email" name="email" placeholder="Email address" required />
-	<small>We'll never share your email with anyone else.</small>
 
-	<!-- Button -->
-	<button type="submit">Submit</button>
-</form>
+	<p>Countries</p>
+	<ul>
+		{#each data.russellbits_messages as message}
+			<li>{message.sender_name}</li>
+			<li>{message.email}</li>
+			<li>{message.message}</li>
+			<li>{message.sent}</li>
+		{/each}
+	</ul>
+
+	<p>Reach out and say Hi.</p>
+
+	<form id="contact-form">
+		<label for="email"
+			>Email address:
+			<input type="email" id="email" name="email" placeholder="Email address" required />
+		</label>
+		<label for="name"
+			>Name:
+			<input
+				type="text"
+				id="name"
+				name="sender_name"
+				placeholder="Your name"
+				class="textfield"
+				required
+			/>
+		</label>
+
+		<p>
+			<small
+				>I'll never share your email with anyone else and you're not signing up for anything.</small
+			>
+		</p>
+		<label for="message"
+			>Your message:
+			<input type="textarea" id="message" name="message" placeholder="Your message" required />
+		</label>
+		<input type="hidden" name="sent" value="2024-08-09T12:04:38Z" />
+		<!-- Button -->
+		<button type="submit">Submit</button>
+	</form>
+</section>
+
+<style lang="scss">
+	section {
+		width: 50vw;
+		margin: 0 auto;
+		border: 1px solid aqua;
+	}
+	form {
+		color: white;
+		display: flex;
+		flex-direction: column;
+		gap: 1em;
+	}
+</style>
