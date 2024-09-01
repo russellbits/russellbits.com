a50de4020ee0aa78d821cabb9d93c2f847fa6085
Russell Warner <rewarner@russellbits.com>
2024-09-01 00:13:49 -0400

    Experimenting with animation on major components.

--- src/routes/contact/+page.server.js
+++ src/routes/contact/+page.server.js
@@ -1,17 +1,17 @@
 // @ts-ignore
 import { supabase } from '$lib/supabaseClient'
 
-export async function load() {
-	// const { error, data } = await supabase.from('russellbits_messages').select('*').eq('id', 1)
-	const { error, data } = await supabase
-		.from('russellbits_messages')
-		.select('*')
-		.order('id', { ascending: false })
-		.limit(1)
-	console.log('Attempting to receive data...')
-	console.log(error)
-	console.log(data)
-	return {
-		russellbits_messages: data ?? []
-	}
-}
+// export async function load() {
+// 	// const { error, data } = await supabase.from('russellbits_messages').select('*').eq('id', 1)
+// 	const { error, data } = await supabase
+// 		.from('russellbits_messages')
+// 		.select('*')
+// 		.order('id', { ascending: false })
+// 		.limit(1)
+// 	console.log('Attempting to receive data...')
+// 	console.log(error)
+// 	console.log(data)
+// 	return {
+// 		russellbits_messages: data ?? []
+// 	}
+// }
