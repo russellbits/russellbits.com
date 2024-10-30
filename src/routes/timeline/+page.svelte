1e6dfa9979ed1c48c1cc637d092be2a424ffb675
Russell Warner <rewarner@russellbits.com>
2024-10-30 16:24:56 -0400

    New route for timeline page.

---
+++ src/routes/timeline/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Timeline from '$lib/components/Timeline.svelte'
+	import timelineData from '$lib/data/index.js'
+</script>
+
+<main>
+	<Timeline data={timelineData} />
+</main>
+
+<style>
+</style>
