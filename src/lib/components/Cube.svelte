d2fe34d9d5f44cbdf5785ff954d316dbdf9f4d0c
Russell Warner <rewarner@russellbits.com>
2024-12-04 09:35:34 -0500

    New components for the parallax background layer.

---
+++ src/lib/components/Cube.svelte
@@ -0,0 +1,78 @@
+<script>
+	// @ts-nocheck
+	export let color = 'blue'
+	export let size = 'large'
+	let width
+	let height
+	let cubeColors
+
+	if (color == 'purple') {
+		cubeColors = ['#E1E1E1', '#641890', '#B339F9']
+	} else {
+		// it's blue
+		cubeColors = ['#E1E1E1', '#4354AB', '#5970E2']
+	}
+
+	if (size === 'large') {
+		width = 125
+		height = 123
+	} else if (size === 'medium') {
+		width = 93.75
+		height = 92.25
+	} else {
+		// it's small
+		width = 62.5
+		height = 61.5
+	}
+</script>
+
+<!--
+@component
+## Cube
+Creates a decorative element
+
+@example
+```svelte
+```
+
+### Slot props
+- `color` - blue | purple
+- `size` - small | medium | large
+-->
+
+<svg
+	width="{width}px"
+	height="{height}px"
+	viewBox="0 0 125 123"
+	class="cube"
+	version="1.1"
+	xmlns="http://www.w3.org/2000/svg"
+	xmlns:xlink="http://www.w3.org/1999/xlink"
+>
+	<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+		<g transform="translate(-720.000000, -700.000000)">
+			<g transform="translate(719.000000, 700.000000)">
+				<g transform="translate(1.000000, -0.000000)">
+					<polygon fill={cubeColors[0]} points="0 102.309623 60.0348135 80 125 102.309623 62.5 123"
+					></polygon>
+					<polygon fill={cubeColors[1]} points="60 0 125 22.5454545 125 101 60 79.2727273"
+					></polygon>
+					<polygon fill={cubeColors[0]} points="0 22.3096234 60.0348135 0 125 22.3096234 62.5 43"
+					></polygon>
+					<polygon fill={cubeColors[2]} points="0 23 63 43.8711434 63 123 0 102.623851"></polygon>
+				</g>
+			</g>
+		</g>
+	</g>
+</svg>
+
+<style>
+	.cube {
+		position: relative;
+		top: var(--top);
+		right: var(--right);
+		width: 125px;
+		height: 123px;
+		transform: translateZ(var(--distance));
+	}
+</style>
---
+++ src/lib/components/ParallaxLayer.svelte
@@ -0,0 +1,74 @@
+<script>
+	// @ts-nocheck
+	import Cube from '$lib/components/Cube.svelte'
+
+	const generateRandomPositions = (numArrays, arrayLength) => {
+		const result = []
+		for (let i = 0; i < numArrays; i++) {
+			const randomArray = []
+			for (let j = 0; j < arrayLength; j++) {
+				randomArray.push(Math.random() * 100)
+			}
+			result.push(randomArray)
+		}
+		return result
+	}
+	const positionArrays = generateRandomPositions(6, 2)
+</script>
+
+<div class="parallax_layer">
+	<Cube
+		size="large"
+		color="blue"
+		--top="{positionArrays[0][0]}%"
+		--right="{positionArrays[0][1]}%"
+		--distance="-10px"
+	/>
+	<Cube
+		size="large"
+		color="blue"
+		--top="{positionArrays[1][0]}%"
+		--right="{positionArrays[1][1]}%"
+		--distance="-20px"
+	/>
+	<Cube
+		size="large"
+		color="blue"
+		--top="{positionArrays[2][0]}%"
+		--right="{positionArrays[2][1]}%"
+		--distance="-30px"
+	/>
+	<Cube
+		size="large"
+		color="purple"
+		--top="{positionArrays[3][0]}%"
+		--right="{positionArrays[3][1]}%"
+	/>
+	<Cube
+		size="medium"
+		color="purple"
+		--top="{positionArrays[4][0]}%"
+		--right="{positionArrays[4][1]}%"
+	/>
+	<Cube
+		size="small"
+		color="purple"
+		--top="{positionArrays[5][0]}%"
+		--right="{positionArrays[5][1]}%"
+	/>
+</div>
+
+<style>
+	.parallax_layer {
+		position: relative;
+		width: 50vw;
+		height: 100vh;
+		top: 0;
+		left: 50%;
+		transform: translateX(-50%);
+		background-color: rgba(120, 120, 120, 0.5);
+		margin: 0;
+		padding: 0;
+		z-index: 10;
+	}
+</style>
