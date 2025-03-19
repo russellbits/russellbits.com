e75f7bb1fbbdfc76fc27f7ace165b0fb16becc83
Russell Warner <rewarner@russellbits.com>
2025-03-18 21:48:59 -0400

    Added tooltips to main menu navigation

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -40,22 +40,33 @@
 				</li>
 			{/each}
 			<li>
-				<a href="/contact"><i class="icon glyph-mail glyph-white" /></a>
-			</li>
-			<li>
-				<a href="http://github.com/russellbits"><i class="glyph-github glyph-white" /></a>
-			</li>
-			<li>
-				<a href="http://linkedin.com/in/russellbits"><i class="glyph-linkedin glyph-white" /></a>
-			</li>
-			<li>
-				<a href="https://codepen.io/russellbits"
-					><img src={iconCodepen} width="25" height="25" alt="Codepen logo" /></a
+				<span tooltip="Send me a message!" flow="down"
+					><a href="/contact"><i class="icon glyph-mail glyph-white" /></a></span
 				>
 			</li>
 			<li>
-				<a href="http://instagram.com/belovedleader"
-					><i class="icon glyph-instagram glyph-white" /></a
+				<span tooltip="See my code" flow="down"
+					><a href="http://github.com/russellbits"><i class="glyph-github glyph-white" /></a></span
+				>
+			</li>
+			<li>
+				<span tooltip="Network me" flow="down"
+					><a href="http://linkedin.com/in/russellbits"><i class="glyph-linkedin glyph-white" /></a
+					></span
+				>
+			</li>
+			<li>
+				<span tooltip="See my experiments" flow="down"
+					><a href="https://codepen.io/russellbits"
+						><img src={iconCodepen} width="25" height="25" alt="Codepen logo" /></a
+					></span
+				>
+			</li>
+			<li>
+				<span tooltip="See my pictures" flow="down">
+					<a href="http://instagram.com/belovedleader"
+						><i class="icon glyph-instagram glyph-white" /></a
+					></span
 				>
 			</li>
 		</ul>
@@ -140,7 +151,8 @@
 		padding: 0;
 	}
 
-	nav.menu > ul > li > a {
+	nav.menu > ul > li > a,
+	nav.menu > ul > li > span > a {
 		color: #fff;
 		text-decoration: none;
 		display: flex;
@@ -199,7 +211,7 @@
 		align-items: center;
 	}
 
-	ul.mobile-menu > li > a {
+	ul.mobile-menu > li > span > a {
 		font-family: 'Raleway', sans-serif;
 		font-style: bold;
 		font-weight: 700;
@@ -208,7 +220,7 @@
 		padding: 5px;
 	}
 
-	ul.mobile-menu > li > a:hover {
+	ul.mobile-menu > li > span > a:hover {
 		text-decoration: none;
 	}
 
@@ -278,5 +290,156 @@
 		#mobile-menu-control {
 			display: flex;
 		}
+	}
+	/* START TOOLTIP STYLES */
+	[tooltip] {
+		position: relative; /* opinion 1 */
+	}
+
+	/* Applies to all tooltips */
+	[tooltip]::before,
+	[tooltip]::after {
+		text-transform: none; /* opinion 2 */
+		font-size: 0.9em; /* opinion 3 */
+		line-height: 1;
+		user-select: none;
+		pointer-events: none;
+		position: absolute;
+		display: none;
+		opacity: 0;
+	}
+	[tooltip]::before {
+		content: '';
+		border: 5px solid transparent; /* opinion 4 */
+		z-index: 1001; /* absurdity 1 */
+	}
+	[tooltip]::after {
+		content: attr(tooltip); /* magic! */
+
+		/* most of the rest of this is opinion */
+		font-family: Helvetica, sans-serif;
+		text-align: center;
+
+		/* 
+    Let the content set the size of the tooltips 
+    but this will also keep them from being obnoxious
+    */
+		min-width: 3em;
+		max-width: 21em;
+		white-space: nowrap;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		padding: 1ch 1.5ch;
+		border-radius: 0.3ch;
+		box-shadow: 0 1em 2em -0.5em rgba(0, 0, 0, 0.35);
+		background: #333;
+		color: #fff;
+		z-index: 1000; /* absurdity 2 */
+	}
+
+	/* Make the tooltips respond to hover */
+	[tooltip]:hover::before,
+	[tooltip]:hover::after {
+		display: block;
+	}
+
+	/* don't show empty tooltips */
+	[tooltip='']::before,
+	[tooltip='']::after {
+		display: none !important;
+	}
+
+	/* FLOW: UP */
+	[tooltip]:not([flow])::before,
+	[tooltip][flow^='up']::before {
+		bottom: 100%;
+		border-bottom-width: 0;
+		border-top-color: #333;
+	}
+	[tooltip]:not([flow])::after,
+	[tooltip][flow^='up']::after {
+		bottom: calc(100% + 5px);
+	}
+	[tooltip]:not([flow])::before,
+	[tooltip]:not([flow])::after,
+	[tooltip][flow^='up']::before,
+	[tooltip][flow^='up']::after {
+		left: 50%;
+		transform: translate(-50%, -0.5em);
+	}
+
+	/* FLOW: DOWN */
+	[tooltip][flow^='down']::before {
+		top: 100%;
+		border-top-width: 0;
+		border-bottom-color: #333;
+	}
+	[tooltip][flow^='down']::after {
+		top: calc(100% + 5px);
+	}
+	[tooltip][flow^='down']::before,
+	[tooltip][flow^='down']::after {
+		left: 50%;
+		transform: translate(-50%, 0.5em);
+	}
+
+	/* FLOW: LEFT */
+	[tooltip][flow^='left']::before {
+		top: 50%;
+		border-right-width: 0;
+		border-left-color: #333;
+		left: calc(0em - 5px);
+		transform: translate(-0.5em, -50%);
+	}
+	[tooltip][flow^='left']::after {
+		top: 50%;
+		right: calc(100% + 5px);
+		transform: translate(-0.5em, -50%);
+	}
+
+	/* FLOW: RIGHT */
+	[tooltip][flow^='right']::before {
+		top: 50%;
+		border-left-width: 0;
+		border-right-color: #333;
+		right: calc(0em - 5px);
+		transform: translate(0.5em, -50%);
+	}
+	[tooltip][flow^='right']::after {
+		top: 50%;
+		left: calc(100% + 5px);
+		transform: translate(0.5em, -50%);
+	}
+
+	/* KEYFRAMES */
+	@keyframes tooltips-vert {
+		to {
+			opacity: 0.9;
+			transform: translate(-50%, 0);
+		}
+	}
+
+	@keyframes tooltips-horz {
+		to {
+			opacity: 0.9;
+			transform: translate(0, -50%);
+		}
+	}
+
+	/* FX All The Things */
+	[tooltip]:not([flow]):hover::before,
+	[tooltip]:not([flow]):hover::after,
+	[tooltip][flow^='up']:hover::before,
+	[tooltip][flow^='up']:hover::after,
+	[tooltip][flow^='down']:hover::before,
+	[tooltip][flow^='down']:hover::after {
+		animation: tooltips-vert 300ms ease-out forwards;
+	}
+
+	[tooltip][flow^='left']:hover::before,
+	[tooltip][flow^='left']:hover::after,
+	[tooltip][flow^='right']:hover::before,
+	[tooltip][flow^='right']:hover::after {
+		animation: tooltips-horz 300ms ease-out forwards;
 	}
 </style>
